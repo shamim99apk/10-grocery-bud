26567cb2486d3d9c142085eaff7ce855df56b02a
shamim99apk <rianahmedshamim99apk@gmail.com>
2021-02-16 22:23:27 +0600

    first commit

---
+++ src/List.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { FaEdit, FaTrash } from 'react-icons/fa';
+const List = ({ items, editItem, removeItem }) => {
+  return (
+    <div>
+      {items.map((item) => {
+        const { id, title } = item;
+        return (
+          <article className='grocery-item' key={id}>
+            <p className='title'>{title}</p>
+            <div className='btn-container'>
+              <button
+                type='button'
+                className='edit-btn'
+                onClick={() => editItem(id)}>
+                <FaEdit />
+              </button>
+              <button
+                type='button'
+                className='delete-btn'
+                onClick={() => removeItem(id)}>
+                <FaTrash />
+              </button>
+            </div>
+          </article>
+        );
+      })}
+    </div>
+  );
+};
+
+export default List;
